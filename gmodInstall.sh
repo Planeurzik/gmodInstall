8c2f5e7018f3deb5b39717008575f752a680455e
Planeurzik <38111551+Planeurzik@users.noreply.github.com>
2020-05-14 15:41:25 +0200

    Update gmodInstall.sh

--- gmodInstall.sh
+++ gmodInstall.sh
@@ -38,8 +38,7 @@ if (fs.existsSync(downloadFilePath)) { fs.unlinkSync(downloadFilePath); }
 
 fs.readdirSync(__dirname+'/addons').forEach((addon) => {
     if (addon[0] !== '.') {
-        var addonSplit = addon.split('_');
-        var workshopId = addonSplit[addonSplit.length - 1];
+        var workshopId = addon.split('_')[0];
         if (/^\d+$/.test(workshopId)) {
             fs.appendFileSync(downloadFilePath, 'resource.AddWorkshop(\"' + workshopId + '\")\n');
         }
